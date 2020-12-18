4eb58983b39bd043967aee56ec199417156425bd
Jared Henry <jared.klayton.henry@gmail.com>
2020-12-18 14:12:40 -0600

    first commit

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_new_angular_auth_individual.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
